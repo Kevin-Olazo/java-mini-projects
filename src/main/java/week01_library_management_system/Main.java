77e5a68bf6fb8f01505135d33c54f8532457c4f8
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-10 20:33:23 -0500

    feat: main method, created library, book1, book2 and member1

--- src/main/java/week01_library_management_system/Main.java
+++ src/main/java/week01_library_management_system/Main.java
@@ -1,4 +1,17 @@
 package week01_library_management_system;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Library library = new Library();
+
+        Book book1 = new Book("I-H01", "Harry Potter", "J.K. Rowling");
+        Book book2 = new Book("I-LOTR01", "Lord of the Rings", "J.R.R. Tolkien");
+
+        Member member1 = new Member("Alice");
+
+        member1.borrowBook(book1);
+
+
+    }
 }

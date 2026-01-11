0ca61f565f48e52515d62b1a5843a2b25d570abe
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-10 20:57:52 -0500

    feat: used library addBook and registerMember methods correctly

--- src/main/java/week01_library_management_system/Main.java
+++ src/main/java/week01_library_management_system/Main.java
@@ -12,9 +12,16 @@ public class Main {
         Book book3 = new Book("I-GOT01", "Game of Thrones", "George R.R. Martin");
         Book book4 = new Book("I-HG01", "Hunger Games", "Suzanne Collins");
 
+        // Add books to library
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
 
         // 3. Create Member
         Member member1 = new Member("Alice");
+
+        library.registerMember(member1);
 
         // 4. Member borrows books
         member1.borrowBook(book1);
@@ -24,15 +31,14 @@ public class Main {
 
         // 5. Create second member
         Member member2 = new Member("Bob");
+        library.registerMember(member2);
+
         member2.borrowBook(book1); // Can't borrow, book isn't available
 
         member1.returnBook(book1); // returns book, now can borrow again
 
         // Now second can borrow the book
         member2.borrowBook(book1);
-
-
-
 
     }
 }

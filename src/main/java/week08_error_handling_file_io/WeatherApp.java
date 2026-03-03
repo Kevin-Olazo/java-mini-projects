3c375355dfb5b6e7ed1eee523fd03186134df9f2
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-03 13:10:26 -0500

    feat: read data from log file and handle exceptions

--- src/main/java/week08_error_handling_file_io/WeatherApp.java
+++ src/main/java/week08_error_handling_file_io/WeatherApp.java
@@ -1,5 +1,9 @@
 package week08_error_handling_file_io;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
 public class WeatherApp {
     public static void main(String[] args) {
         DataLogger dataLogger = new DataLogger();
@@ -13,6 +17,19 @@ public class WeatherApp {
                 System.out.println(e.getMessage());
             }
 
+        }
+
+
+        try  {
+            List<WeatherReading> listReadings = dataLogger.loadReadings(LocalDate.now());
+            for (WeatherReading r : listReadings){
+                System.out.println(r.toString());
+            }
+
+        } catch (CorruptDataException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }

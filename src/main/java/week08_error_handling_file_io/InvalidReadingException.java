03a7bf616b89eac9b070e582d72d11b8c967b9d5
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-02 19:19:48 -0500

    feat: implement Fail-Fast validation with InvalidReadingException

---
+++ src/main/java/week08_error_handling_file_io/InvalidReadingException.java
@@ -0,0 +1,7 @@
+package week08_error_handling_file_io;
+
+public class InvalidReadingException extends RuntimeException {
+  public InvalidReadingException(String message) {
+    super(message);
+  }
+}
